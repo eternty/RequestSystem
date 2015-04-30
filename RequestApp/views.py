efd2f4490487ba2e01906976f27adb0a62e63053
eternty <eternty1993@gmail.com>
2015-04-30 21:18:22 +0300

    disp.html - is almost ok client.html = ok)  filter for requests for groups of eng -ok  new base-nav for baseconsole -ok  companypage - ok!  new superbasetemplate)
     troubles with get_full_name in companypage.html
     css modernizations remain

--- RequestApp/views.py
+++ RequestApp/views.py
@@ -9,23 +9,28 @@ from RequestApp.models import User_type, Company, Request, Request_status, Speci
 
 @login_required(login_url='/signin')
 def index(request):
+    usertype = request.user.usertype.name
     if request.user.usertype.name == 'Engineer':
 
         requests = Request.objects.filter(group__in=request.user.get_specialization())
         myrequests = Request.objects.filter(engineer = request.user)
+
         context = {
             'requests': requests,
-            'myrequests': myrequests
+            'myrequests': myrequests,
+            'usertype': usertype
         }
         return render(request, 'engineer.html', context )
 
     elif request.user.usertype.name == 'Dispatcher':
 
+
         requests = Request.objects.filter(status__id = 2 )
         myrequests = Request.objects.filter(engineer = request.user)
         context = {
             'requests': requests,
-            'myrequests': myrequests
+            'myrequests': myrequests,
+             'usertype': usertype
         }
         return render(request, 'disp.html', context)
 
@@ -35,7 +40,8 @@ def index(request):
         myrequests = Request.objects.filter(creator = request.user)
         context = {
             'requests': requests,
-            'myrequests': myrequests
+            'myrequests': myrequests,
+            'usertype': usertype
         }
         return render(request, 'client.html', context)
     else:
@@ -43,7 +49,8 @@ def index(request):
         newrequests = Request.objects.filter(status__id = 2)
         context = {
             'requests': requests,
-            'newrequests': newrequests
+            'newrequests': newrequests,
+            'usertype': usertype
         }
         return render(request, 'manager.html', context)
 
@@ -56,10 +63,20 @@ def signin(request):
         login(request, user)
         return redirect("/")
     else:
-        return render(request, "SignIn.html" )
+        return render(request, 'SignIn.html' )
 
 def hello(request):
 	return HttpResponse("Hey You must be serious man, huh?")
 
 def companypage(request):
-    return render(request, "companypage.html")
+    usertype = request.user.usertype.name
+    clientcompany = Company.objects.get(name = request.user.company)
+    manager = clientcompany.manager.get_full_name()
+    focus = clientcompany.focus.get_full_name()
+    context = {
+            'clientcompany': clientcompany,
+            'usertype': usertype,
+            'manager': manager,
+            'focus': focus
+        }
+    return render(request, 'companypage.html', context)
